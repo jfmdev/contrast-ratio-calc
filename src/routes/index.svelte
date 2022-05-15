6f8e88c46b330f385712baceef7bb737a171613b
Jose <jfmdev@gmail.com>
2022-05-15 18:59:49 -0300

    Configure SASS preprocessor and add Bootstrap and Color dependencies

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,35 @@
+<script>
+  import '../styles/main.scss';
+  import Color from 'color';
+
+  let x = 1;
+  let y = 2;
+
+  const color = Color.rgb(255, 128, 255);
+
+  function yPlusAValue(value) {
+    return value + y;
+  }
+
+  $: total = yPlusAValue(x);
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p class="text-test">{x} and {y} = {color.hsl()}</p>
+
+<div class="container text-center">
+  <div class="row">
+    <div class="col">Column</div>
+    <div class="col">Column</div>
+    <div class="col">Column</div>
+  </div>
+</div>
+
+<style lang="scss">
+  $test-color: #0080ff;
+
+  h1 {
+    color: $test-color;
+  }
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+import preprocessor from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
+  preprocess: preprocessor(),
+  kit: {
+    adapter: adapter({
+      fallback: 'index.html'
+    })
+  }
 };
 
 export default config;
