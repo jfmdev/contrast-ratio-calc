55f4e182b3b6163d7fd10fd0522ea52df8b85122
Jose <jfmdev@gmail.com>
2022-05-21 18:33:40 -0300

    Implement "Improve" button to suggest colors with better contrast

--- src/widgets/color-input.svelte
+++ src/widgets/color-input.svelte
@@ -6,6 +6,9 @@
 
   // --- Variables --- //
 
+  // Constants.
+  const MAX_RATIO = 21;
+
   // Properties.
   export let active;
   export let available;
@@ -34,15 +37,6 @@
 
   // --- Functions --- //
 
-  function onInput() {
-    active = parsedInput;
-  }
-
-  // TODO: Implement (improve contrast to 3, 4.5, 6, 7.5...)
-  function improve() {
-    alert('WIP');
-  }
-
   function addColor() {
     if (alreadyAdded) {
       return;
@@ -55,6 +49,45 @@
     }
 
     available = available;
+  }
+
+  /**
+   * Search for a color with same hue and saturation but with different luminosity to achieve better contrast.
+   */
+  function improve() {
+    const currRatio = active.contrast(partner);
+    if (!active || !partner || currRatio >= MAX_RATIO) {
+      return;
+    }
+
+    // Attempt to improve contrast by 0.5.
+    let nextRatio = Math.ceil((currRatio + 0.1) / 0.5) * 0.5;
+    if (nextRatio >= MAX_RATIO) {
+      nextRatio = MAX_RATIO;
+    }
+
+    let nextColor = Color(active.hex());
+    let nextLightness = 50;
+
+    while (nextColor.contrast(partner) < nextRatio && nextLightness > 0 && nextLightness < 100) {
+      const hslValues = nextColor.hsl().color;
+      nextLightness = hslValues[2] + (partner.isDark() ? 1 : -1);
+
+      if (nextLightness > 100) nextLightness = 100;
+      if (nextLightness < 0) nextLightness = 0;
+
+      nextColor = Color(`hsl(${hslValues[0]}, ${hslValues[1]}%, ${nextLightness}%)`);
+    }
+
+    // Check that color was indeed improved.
+    if (currRatio < nextColor.contrast(partner)) {
+      active = nextColor;
+      inputText = nextColor.hex();
+    }
+  }
+
+  function onInput() {
+    active = parsedInput;
   }
 
   function safeParseColor(textColor) {
